3eaf607f14bc790feaf5b0c73f69b87c37030663
Miguel Peña <miguelp1986@gmail.com>
2023-11-08 21:53:56 -0800

    updated gh actions

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Load environment variables and run the containers
-source source scripts/run.sh
+source scripts/run.sh
 
 # Run tests
 docker compose -f "$full_docker_compose_path" exec web flake8 /app
