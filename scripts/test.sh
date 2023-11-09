62362a4a52a1f1fd10ce00aa1d35d9e4d5f5e1e9
Miguel Peña <miguelp1986@gmail.com>
2023-11-08 21:44:57 -0800

    updated gh actions

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 
-# Load environment variables
-source scripts/load_env.sh
-
-# Construct the full path to the Docker compose file
-docker_compose_file="docker-compose.yml"
-full_docker_compose_path="$parent_dir/$docker_compose_file"
-
-# Check if the Docker Compose file exists
-if [ ! -f "$full_docker_compose_path" ]; then
-  echo "Docker Compose file not found: $full_docker_compose_path"
-  exit 1
-fi
+# Load environment variables and run the containers
+source source scripts/run.sh
 
 # Run tests
 docker compose -f "$full_docker_compose_path" exec web flake8 /app
