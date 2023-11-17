b604067ff38f47a95345c8872e3273a6f99de83c
Miguel Peña <miguelp1986@gmail.com>
2023-11-16 22:17:44 -0800

    added logger

--- src/api/api.py
+++ src/api/api.py
@@ -1,8 +1,15 @@
+
 from fastapi import FastAPI
+from logger import setup_logger
 
 app = FastAPI()
+
+# Create a logger
+logger = setup_logger()
 
 
 @app.get("/")
 def read_root():
+    """."""
+    logger.info("Mic check")
     return {"Mic check": 12}
