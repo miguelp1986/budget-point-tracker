6dcb811a5f13d6bc87c20b809bfe9fcb93205ce1
Miguel Peña <miguelp1986@gmail.com>
2023-12-07 01:29:47 -0800

    updated gh actions testing

---
+++ scripts/set_gh_secrets.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# This script sets GitHub secrets from an .env file
+while IFS= read -r line; do
+    # Skip empty lines and lines starting with #
+    if [[ -z "$line" || "$line" == \#* ]]; then
+        continue
+    fi
+
+    # Splitting line into key and value
+    IFS='=' read -r key value <<< "$line"
+    
+    # Set the GitHub secret
+    gh secret set "$key" --body "$value"
+done < .env
+
