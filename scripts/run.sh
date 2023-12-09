ed1038d4e4d09b7fd5a2d2cc2b6e9fdbb79d5bff
Miguel Peña <miguelp1986@gmail.com>
2023-12-08 23:56:37 -0800

    env file and docker file path fixes

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,15 +3,5 @@
 # Load environment variables
 source scripts/load_env.sh
 
-# Construct the full path to the Docker compose file
-docker_compose_file="docker-compose.yml"
-full_docker_compose_path="$parent_dir/$docker_compose_file"
-
-# Check if the Docker Compose file exists
-if [ ! -f "$full_docker_compose_path" ]; then
-  echo "Docker Compose file not found: $full_docker_compose_path"
-  exit 1
-fi
-
 # Build and start the containers
-docker compose -f "$full_docker_compose_path" up -d --build
+docker compose up -d --build
